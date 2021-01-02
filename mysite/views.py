8e5b5a972ab4ebd7c9b9599e5a8f596c184fb42b
formarx <formarx@gmail.com>
2021-01-02 20:51:14 +0900

    home.html

--- mysite/views.py
+++ mysite/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from polls import views as pollsViews
+from polls import models as pollsModels
 from history import views as historyViews
 from history import models as historyModels
 
@@ -17,5 +17,9 @@ class MainView(historyViews.IndexView):
 def MainPage(request):
     history_list = historyModels.History.objects.all().order_by('-history_date')[:20]
     context = {'history_list': history_list, }
+
+    latest_question_list = pollsModels.Question.objects.all().order_by('-pub_date')[:5]
+    context['latest_question_list'] = latest_question_list
+
     return render(request, 'home.html', context)
 
